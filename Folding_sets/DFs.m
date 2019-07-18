e767462ee087b3066c3e3914c3288aa23cac7e98
JJWK <JJWK@users.noreply.github.com>
2019-07-18 14:30:41 -0300

    actualizacion de folding sets en matlab - foldings de 128

---
+++ Folding_sets/DFs.m
@@ -0,0 +1,16 @@
+scriptname = 'folding_simple128puntos.m'
+run(scriptname);
+k=128;
+we_ab1=zeros(1,128);
+
+P_b=0;
+P_c=0;
+P_d=0;
+P_e=0;
+P_f=0;
+P_g=0;
+
+we_i=1;
+z=1;
+j=1;
+DF_A_B_1=k*we_ab1(we_i)-P_b+B((z))-A(j);
--- Folding_sets/folding_simple128puntos.m
+++ Folding_sets/folding_simple128puntos.m
@@ -1,29 +1,129 @@
 
 %%%%%
 A =[];
-
-
- for i=64:128
-    A(i)=i-1; 
+ for i=0:127
+    A(i+1)=i+1; 
  end    
-
  
- B=[];
- for i=1:32
-   B(33-i)=64-i; 
+%%%%% 
+B=[];
+ for i=32:63
+   B(abs(32-i-1))=i; 
  end    
 
  for i=96:128
-   B(i)=i-96; 
+   B(abs(96-32-1-i))=i; 
  end    
 
- 
+  for i=0:31
+   B(abs(0-64-1-i))=i; 
+  end    
+
+   for i=64:95
+   B(abs(64-96-1-i))=i; 
+  end    
+
+  %%%%% 
+C=[];
+ for i=64:95
+   C(abs(64-i-1))=i; 
+    end    
+
+ for i=32:63
+   C(abs(96-96-1-i))=i;
+   
+ end    
+% 
+  for i=96:127
+   C(abs(64-32-1-i))=i;
+   
+  end    
+% 
+   for i=0:31
+   C(abs(0-96-1-i))=i;
+    end    
+
   
- C=[];
- for i=1:48
-   C(49-i)=80-i; 
+   %%%%% 
+D=[];
+ for i=0:31
+   D(abs(i+1))=i; 
+    end    
+
+ for i=64:95
+   D(abs(32-1-i))=i;
+   
+ end    
+% 
+  for i=32:63
+   D(abs(0-32-1-i))=i;
+   
+  end    
+% 
+   for i=96:127
+   D(abs(i+1))=i;
+   end    
+
+%%%%%%    
+   E=[];
+ for i=96:127
+   E(abs(96-1-i))=i; 
+    end    
+
+ for i=0:31
+   E(abs(32+1+i))=i;
+   
+ end    
+% 
+  for i=64:95
+   E(abs(1+i))=i;
+   
+  end    
+% 
+   for i=32:63
+   E(abs(i+1+64))=i;
+   end    
+  
+    
+   
+   %%%%% 
+F=[];
+ for i=32:63
+   F(abs(32-i-1))=i; 
  end    
 
-%  for i=81:112
-%    C(i)=i-80; 
-%  end    
+ for i=96:128
+   F(abs(96-32-1-i))=i; 
+ end    
+
+  for i=0:31
+   F(abs(0-64-1-i))=i; 
+  end    
+
+   for i=64:95
+   F(abs(64-96-1-i))=i; 
+  end    
+   
+      
+   
+%%%%%%    
+   G=[];
+ for i=96:127
+   E(abs(96-1-i))=i; 
+    end    
+
+ for i=0:31
+   G(abs(32+1+i))=i;
+   
+ end    
+% 
+  for i=64:95
+   G(abs(1+i))=i;
+   
+  end    
+% 
+   for i=32:63
+   G(abs(i+1+64))=i;
+   end    
+  
+   
