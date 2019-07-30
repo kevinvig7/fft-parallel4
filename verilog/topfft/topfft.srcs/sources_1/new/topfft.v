75a08b814628a4abbe8c79854d2049768c341baa
JJWK <JJWK@users.noreply.github.com>
2019-07-30 17:46:53 -0300

    Verilog modificado y esquema nuevo
    Verilog modificado con coeficientes 1
    estudiando los coeficientes y los switches en inkscape

--- verilog/topfft/topfft.srcs/sources_1/new/topfft.v
+++ verilog/topfft/topfft.srcs/sources_1/new/topfft.v
@@ -32,6 +32,13 @@ module topfft(
     );
 
   parameter Nbits = `Nbitsg;
+  
+  parameter w0=1;
+  parameter w1=1;
+  parameter w2=1;
+  parameter w3=1;
+  parameter w4=1;
+  parameter w5=1;
 
     input [Nbits*2-1:0] fftIn_up;
     input  [Nbits*2-1:0] fftIn_down;
@@ -41,26 +48,55 @@ module topfft(
     input rst;
     input ctrl;
     
-  wire [Nbits*2-1:0] blq_connect_up[0:2];
-  wire [Nbits*2-1:0] blq_connect_down[0:2];
+  wire [Nbits*2-1:0] blq_connect_up[0:13];
+  wire [Nbits*2-1:0] blq_connect_down[0:13];
   
   
   assign blq_connect_up[0] = fftIn_up;
   assign blq_connect_down[0] = fftIn_down;
   
-  assign blq_connect_up[2] = fftOut_up;
-  assign blq_connect_down[2] = fftOut_down;
+  assign blq_connect_up[13] = fftOut_up;
+  assign blq_connect_down[13] = fftOut_down;
   
-  Blq#(32,32) BFI(blq_connect_up[0],blq_connect_down[0],blq_connect_up[1],blq_connect_down[1],rst,clk,ctrl);
+  Blq#(128,128) BFI(blq_connect_up[0],blq_connect_down[0],blq_connect_up[1],blq_connect_down[1],rst,clk,ctrl);
   
+  Blq#(64,64) BFII(blq_connect_up[1],blq_connect_down[1],blq_connect_up[2],blq_connect_down[2],rst,clk,ctrl);
+  
+    //producto 0 
+ assign blq_connect_up[3] = blq_connect_up[2]; 
+ assign blq_connect_down[3] =  blq_connect_down[2]*w0;   
     
-  Blqj#(16,16) BFII(blq_connect_up[1],blq_connect_down[1],blq_connect_up[2],blq_connect_down[2],rst,clk,ctrl);
-  
-  
-  
-   
-  
-  
+ Blq#(32,32) BFIII(blq_connect_up[3],blq_connect_down[3],blq_connect_up[4],blq_connect_down[4],rst,clk,ctrl);   
+    
+     //producto 1 2
+ assign blq_connect_up[5] = blq_connect_up[4]*w1; 
+ assign blq_connect_down[5] =  blq_connect_down[4]*w2;   
+    
+     
+ Blq#(16,16) BFIV(blq_connect_up[5],blq_connect_down[5],blq_connect_up[6],blq_connect_down[6],rst,clk,ctrl);   
+ Blq#(8,8) BFV(blq_connect_up[6],blq_connect_down[6],blq_connect_up[7],blq_connect_down[7],rst,clk,ctrl);   
+    
+       //producto 3  
+ assign blq_connect_up[8] = blq_connect_up[7]; 
+ assign blq_connect_down[8] =  blq_connect_down[7]*w3;   
+    
+ Blq#(4,4) BFVI(blq_connect_up[8],blq_connect_down[8],blq_connect_up[9],blq_connect_down[9],rst,clk,ctrl);     
+    
+   //producto 4 5
+ assign blq_connect_up[10] = blq_connect_up[9]*w4; 
+ assign blq_connect_down[10] =  blq_connect_down[9]*w5;   
+    
+    
+    
+ Blq#(2,2) BFVII(blq_connect_up[10],blq_connect_down[10],blq_connect_up[11],blq_connect_down[11],rst,clk,ctrl);      
+ Blq#(1,1) BFVIII(blq_connect_up[11],blq_connect_down[11],blq_connect_up[12],blq_connect_down[12],rst,clk,ctrl);      
+    
+    
+ Blq#(128,128) BFIX(blq_connect_up[12],blq_connect_down[12],blq_connect_up[13],blq_connect_down[13],rst,clk,ctrl); 
+    
+    
+    
+    
     
     
 endmodule
