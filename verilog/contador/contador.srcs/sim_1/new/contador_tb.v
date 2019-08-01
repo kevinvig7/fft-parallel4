25f01cf98790ec4790b63f1be93d49b73aef7efd
JJWK <JJWK@users.noreply.github.com>
2019-08-01 13:14:16 -0300

    verilog actualizado
    agregado el verilog con contador y rst -  faltan como ingresar los coeficientes

--- verilog/contador/contador.srcs/sim_1/new/contador_tb.v
+++ verilog/contador/contador.srcs/sim_1/new/contador_tb.v
@@ -24,25 +24,30 @@ module contador_tb();
 //-- Registro para generar la señal de reloj
 reg clk = 0;
 wire clk_out;
+reg rst;
     
 //-- Instanciar el componente y establecer el valor del divisor
-contador #(8)
+contador #(4)
   dut(
     .clk_in(clk),
+    .rst(rst),
     .clk_out(clk_out)
   );
     
 //-- Generador de reloj. Periodo 2 unidades
 always #1 clk = ~clk;
+
+
     
 //-- Proceso al inicio
 initial begin
-    
+    rst  = 1'd1;
+    #20 rst  = 1'd0;
   //-- Fichero donde almacenar los resultados
   $dumpfile("contador_tb.vcd");
   $dumpvars(0, contador_tb);
     
-  # 120 $display("FIN de la simulacion");
+  # 200 $display("FIN de la simulacion");
   $finish;
 end
 endmodule
--- verilog/topfft/topfft.srcs/sources_1/new/topfft.v
+++ verilog/topfft/topfft.srcs/sources_1/new/topfft.v
@@ -27,8 +27,7 @@ module topfft(
     fftOut_up,
     fftOut_down,
     clk,
-    rst,
-    ctrl
+    rst
     );
 
   parameter Nbits = `Nbitsg;
@@ -38,7 +37,6 @@ module topfft(
   parameter w2=1;
   parameter w3=1;
   parameter w4=1;
-  parameter w5=1;
 
     input [Nbits*2-1:0] fftIn_up;
     input  [Nbits*2-1:0] fftIn_down;
@@ -46,54 +44,46 @@ module topfft(
     output [Nbits*2-1:0] fftOut_down;
     input clk;
     input rst;
-    input ctrl;
     
-  wire [Nbits*2-1:0] blq_connect_up[0:13];
-  wire [Nbits*2-1:0] blq_connect_down[0:13];
-  
-  
+  wire [Nbits*2-1:0] blq_connect_up[0:7];
+  wire [Nbits*2-1:0] blq_connect_down[0:7];
+  wire ctrl_1,ctrl_2,ctrl_4;
+    
   assign blq_connect_up[0] = fftIn_up;
   assign blq_connect_down[0] = fftIn_down;
   
-  assign blq_connect_up[13] = fftOut_up;
-  assign blq_connect_down[13] = fftOut_down;
+  assign blq_connect_up[7] = fftOut_up;
+  assign blq_connect_down[7] = fftOut_down;
   
-  Blq#(128,128) BFI(blq_connect_up[0],blq_connect_down[0],blq_connect_up[1],blq_connect_down[1],rst,clk,ctrl);
+ contador#(4) control_4(clk,rst,ctrl_4);
+ contador#(2) control_2(clk,rst,ctrl_2); 
+ contador#(1) control_1(clk,rst,ctrl_1); 
+
   
-  Blq#(64,64) BFII(blq_connect_up[1],blq_connect_down[1],blq_connect_up[2],blq_connect_down[2],rst,clk,ctrl);
+  Blq#(4,4) BFI(blq_connect_up[0],blq_connect_down[0],blq_connect_up[1],blq_connect_down[1],rst,clk,ctrl_4);
   
-    //producto 0 
- assign blq_connect_up[3] = blq_connect_up[2]; 
- assign blq_connect_down[3] =  blq_connect_down[2]*w0;   
-    
- Blq#(32,32) BFIII(blq_connect_up[3],blq_connect_down[3],blq_connect_up[4],blq_connect_down[4],rst,clk,ctrl);   
-    
+  
+ //producto 0 
+ assign blq_connect_up[2] = blq_connect_up[1]; 
+ assign blq_connect_down[2] =  blq_connect_down[1]*w0;   
+ 
+   Blq#(2,2) BFII(blq_connect_up[2],blq_connect_down[2],blq_connect_up[3],blq_connect_down[3],rst,clk,ctrl_2);
+  
      //producto 1 2
- assign blq_connect_up[5] = blq_connect_up[4]*w1; 
- assign blq_connect_down[5] =  blq_connect_down[4]*w2;   
+ assign blq_connect_up[4] = blq_connect_up[3]*w1; 
+ assign blq_connect_down[4] =  blq_connect_down[3]*w2;   
+    
+        
+ Blq#(1,1) BFIII(blq_connect_up[4],blq_connect_down[4],blq_connect_up[5],blq_connect_down[5],rst,clk,ctrl_1);   
+    
+     //producto 3 4
+ assign blq_connect_up[6] = blq_connect_up[5]*w3; 
+ assign blq_connect_down[6] =  blq_connect_down[5]*w4;   
     
      
- Blq#(16,16) BFIV(blq_connect_up[5],blq_connect_down[5],blq_connect_up[6],blq_connect_down[6],rst,clk,ctrl);   
- Blq#(8,8) BFV(blq_connect_up[6],blq_connect_down[6],blq_connect_up[7],blq_connect_down[7],rst,clk,ctrl);   
-    
-       //producto 3  
- assign blq_connect_up[8] = blq_connect_up[7]; 
- assign blq_connect_down[8] =  blq_connect_down[7]*w3;   
-    
- Blq#(4,4) BFVI(blq_connect_up[8],blq_connect_down[8],blq_connect_up[9],blq_connect_down[9],rst,clk,ctrl);     
-    
-   //producto 4 5
- assign blq_connect_up[10] = blq_connect_up[9]*w4; 
- assign blq_connect_down[10] =  blq_connect_down[9]*w5;   
-    
-    
-    
- Blq#(2,2) BFVII(blq_connect_up[10],blq_connect_down[10],blq_connect_up[11],blq_connect_down[11],rst,clk,ctrl);      
- Blq#(1,1) BFVIII(blq_connect_up[11],blq_connect_down[11],blq_connect_up[12],blq_connect_down[12],rst,clk,ctrl);      
-    
-    
- Blq#(128,128) BFIX(blq_connect_up[12],blq_connect_down[12],blq_connect_up[13],blq_connect_down[13],rst,clk,ctrl); 
-    
+ Blq#(4,4) BFIV(blq_connect_up[6],blq_connect_down[6],blq_connect_up[7],blq_connect_down[7],rst,clk,ctrl_4);   
+ 
+  
     
     
     
