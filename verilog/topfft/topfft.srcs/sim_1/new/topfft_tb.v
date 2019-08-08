64643e8ae20fc580f558f739278ea462d08d60d1
JJWK <JJWK@users.noreply.github.com>
2019-08-08 15:35:59 -0300

    trabajando en el tb
    Agregado el levantamiento de archivos .dat

--- verilog/topfft/topfft.srcs/sim_1/new/topfft_tb.v
+++ verilog/topfft/topfft.srcs/sim_1/new/topfft_tb.v
@@ -26,11 +26,12 @@ module topfft_tb();
  parameter N=8;   
 reg clk = 0;
 reg rst;  
+reg rst_tb;
  
  reg [Nbits*2-1:0] fftIn_up;
  reg [Nbits*2-1:0] fftIn_down;
- wire [Nbits*2-1:0] fftOut_up;
- wire [Nbits*2-1:0] fftOut_down;
+wire [Nbits*2-1:0] fftOut_up;
+wire [Nbits*2-1:0] fftOut_down;
     
        //-- Generador de reloj. Periodo 2 unidades
 always #1 clk = ~clk;
@@ -47,20 +48,62 @@ topfft
     
 
 
-    //-- Proceso al inicio
-initial begin
-    rst  = 1'd1;
-    #20 rst  = 1'd0;
-    #10 fftIn_up ={Nbits*2{1'b1}};
-        fftIn_down ={Nbits*2{1'b1}};
+//    //-- Proceso al inicio
+//initial begin
+//    rst  = 1'd1;
+//    #20 rst  = 1'd0;
+//    #10 fftIn_up ={Nbits*2{1'b1}};
+//        fftIn_down ={Nbits*2{1'b1}};
         
-  //-- Fichero donde almacenar los resultados
-  $dumpfile("topfft_tb.vcd");
-  $dumpvars(0, topfft_tb);
+//  //-- Fichero donde almacenar los resultados
+//  $dumpfile("topfft_tb.vcd");
+//  $dumpvars(0, topfft_tb);
     
-  # 500000 $display("FIN de la simulacion");
-  $finish;
+//  # 500000 $display("FIN de la simulacion");
+//  $finish;
+//end
+    
+integer data_par; // file handler
+integer data_impar;
+
+integer scan_par; // file handler
+integer scan_impar; // file handler
+
+reg [Nbits*2-1:0] captured_data;
+
+`define NULL 0
+
+initial begin
+data_par = $fopen("Entrada_parFFT.dat", "r");
+data_impar = $fopen("Entrada_imparFFT.dat", "r");
+
+        rst_tb  = 1'd1;
+    #20 rst_tb  = 1'd0;
+
+    if (data_par == `NULL) begin
+        $display("data_file handle was NULL");
+         $finish;
+    end
 end
-    
-    
+
+always @(posedge clk) begin
+   if(rst_tb) begin
+  rst  = 1'd1;
+  #20 rst  = 1'd0;
+    end else begin
+        scan_par = $fscanf(data_par, "%b\n", fftIn_up);
+        scan_impar = $fscanf(data_impar, "%b\n", fftIn_down);
+          if ($feof(data_par)) begin
+          # 100
+           $finish;
+         
+          end
+      end
+
+end
+
+
+
+
+
 endmodule
--- verilog/topfft/topfft.srcs/sources_1/new/coeff_data1.v
+++ verilog/topfft/topfft.srcs/sources_1/new/coeff_data1.v
@@ -31,12 +31,12 @@ output reg [Nbits*N*2-1:0] coeff_data;
 initial begin
 coeff_data[31:28] <=4'b0000; 
 coeff_data [27:24]<=4'b0001;  
-coeff_data[23:20] <=4'b0010; 
-coeff_data[19:16] <=4'b0011;  
-coeff_data[15:12] <= 4'b0100; 
-coeff_data[11:8] <= 4'b0101; 
-coeff_data[7:4]<= 4'b0110; 
-coeff_data[3:0] <= 4'b0111; 
+coeff_data[23:20] <=4'b0000; 
+coeff_data[19:16] <=4'b0001;  
+coeff_data[15:12] <= 4'b0000; 
+coeff_data[11:8] <= 4'b0001; 
+coeff_data[7:4]<= 4'b0000; 
+coeff_data[3:0] <= 4'b0001; 
 end 
 endmodule
 
