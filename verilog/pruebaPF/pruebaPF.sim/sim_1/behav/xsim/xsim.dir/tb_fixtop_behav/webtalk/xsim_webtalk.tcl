b831e47f626b2e484fda229d29e0e8569484dbda
JJWK <JJWK@users.noreply.github.com>
2019-08-30 16:15:37 -0300

    boque sat
    ordenando bloque sat.v anda correctamente

--- verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/xsim_webtalk.tcl
+++ verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/fft-parallel4/verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Fri Aug 30 16:04:19 2019" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Fri Aug 30 16:15:00 2019" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.1.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2552052" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "37ec1854-87c9-40b8-91c0-f73b8b213278" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "144fff327ad645088bda81066f1e568f" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "110" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "116" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-6700 CPU @ 3.40GHz" -context "user_environment"
@@ -36,7 +36,7 @@ webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "17" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "3" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip unimacro_ver unisims_ver " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "0.69_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "41252_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1941099464 -regid "" -xml C:/fft-parallel4/verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/fft-parallel4/verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/fft-parallel4/verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "0.67_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "41464_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 279625743 -regid "" -xml C:/fft-parallel4/verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/fft-parallel4/verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/fft-parallel4/verilog/pruebaPF/pruebaPF.sim/sim_1/behav/xsim/xsim.dir/tb_fixtop_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
