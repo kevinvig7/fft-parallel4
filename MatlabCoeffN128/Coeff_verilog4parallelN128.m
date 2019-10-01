b4dd1a1bacbcf8b4dcb71fdb17f432c6ec3195c8
JJWK <JJWK@users.noreply.github.com>
2019-10-01 17:22:41 -0300

    trabajando en el camino de sat 0 a sat 1 - verificar coeficientes con Daniel.

--- MatlabCoeffN128/Coeff_verilog4parallelN128.m
+++ MatlabCoeffN128/Coeff_verilog4parallelN128.m
@@ -9,6 +9,7 @@ Stage1 = textscan(fid,'%s');
 
 
 %  break;
+%%Stage 1
   
 for i=0 : N-1
 coeff0_0(i+1,:)=Stage1{1,1}{2+i*4,1};
@@ -50,14 +51,15 @@ fprintf(fid,['end ' , '\n']);
 fprintf(fid,['endmodule ' , '\n']);
 fclose(fid);
 
-
+%  break;
+%%%%%%%Stage 2
 
 fid = fopen('RotadorStage2.dat','r');
 Stage2 = textscan(fid,'%s');
 
 
-%  break;
-  
+
+
 for i=0 : N-1
 coeff1_0(i+1,:)=Stage2{1,1}{2+i*4,1};
 end
@@ -65,7 +67,6 @@ end
 for i=0 : N-1
 coeff1_1(i+1,:)=Stage2{1,1}{3+i*4,1};
 end
-
 
 for i=0 : N-1
 coeff1_2(i+1,:)=Stage2{1,1}{4+i*4,1};
@@ -113,6 +114,102 @@ end
 fprintf(fid,['end ' , '\n']);
 fprintf(fid,['endmodule ' , '\n']);
 fclose(fid);
+
+
+%%%%%%%%%Stage 3
+
+fid = fopen('RotadorStage3.dat','r');
+Stage3 = textscan(fid,'%s');
+%%%%%%Verificar esto....
+  
+for i=0 : N-1
+coeff2_0(i+1,:)=Stage3{1,1}{1+i*4,1};
+end
+
+for i=0 : N-1
+coeff2_1(i+1,:)=Stage3{1,1}{1+i*4,1};
+end
+
+for i=0 : N-1
+coeff2_2(i+1,:)=Stage3{1,1}{1+i*4,1};
+end
+
+for i=0 : N-1
+coeff2_3(i+1,:)=Stage3{1,1}{1+i*4,1};
+end
+
+
+%%%%%%%%%%%%%%%%%
+fid=fopen ('C:\fft-parallel4\verilog\topfftsrcN128\v\coeff_data2_0.v','wt'); %guardo un archivo con los coef. cuantizados de la señal filtrada de salida
+fprintf(fid,['`timescale 1ns / 1ps' , '\n']);
+fprintf(fid,['module coeff_data2_0', '\n']);
+fprintf(fid,['#(parameter NBITS=',num2str(NBITScoeff),',','\n']);
+fprintf(fid,['  parameter N=',num2str(N),')\n']);
+fprintf(fid,['(output reg [NBITS*N*2-1:0] coeff_data);','\n']);
+fprintf(fid,['initial begin' , '\n']);
+for index=1 : N
+    fprintf( fid , ['coeff_data[' num2str(N*NBITScoeff*2-1-(index-1)*NBITScoeff*2) ' : ' num2str(N*NBITScoeff*2-(index-1)*NBITScoeff*2-NBITScoeff*2) '] = ' num2str(NBITScoeff*2) '''b' coeff1_0(index,:) ';','\n']); 
+end
+fprintf(fid,['end ' , '\n']);
+fprintf(fid,['endmodule ' , '\n']);
+fclose(fid);
+%%%%%%%%%%%%%%%%%
+fid=fopen ('C:\fft-parallel4\verilog\topfftsrcN128\v\coeff_data2_1.v','wt'); %guardo un archivo con los coef. cuantizados de la señal filtrada de salida
+fprintf(fid,['`timescale 1ns / 1ps' , '\n']);
+fprintf(fid,['module coeff_data2_1', '\n']);
+fprintf(fid,['#(parameter NBITS=',num2str(NBITScoeff),',','\n']);
+fprintf(fid,['  parameter N=',num2str(N),')\n']);
+fprintf(fid,['(output reg [NBITS*N*2-1:0] coeff_data);','\n']);
+fprintf(fid,['initial begin' , '\n']);
+for index=1 : N
+    fprintf( fid , ['coeff_data[' num2str(N*NBITScoeff*2-1-(index-1)*NBITScoeff*2) ' : ' num2str(N*NBITScoeff*2-(index-1)*NBITScoeff*2-NBITScoeff*2) '] = ' num2str(NBITScoeff*2) '''b' coeff1_1(index,:) ';','\n']); 
+end
+fprintf(fid,['end ' , '\n']);
+fprintf(fid,['endmodule ' , '\n']);
+fclose(fid);
+%%%%%%%%%%%%%%%%%
+fid=fopen ('C:\fft-parallel4\verilog\topfftsrcN128\v\coeff_data2_2.v','wt'); %guardo un archivo con los coef. cuantizados de la señal filtrada de salida
+fprintf(fid,['`timescale 1ns / 1ps' , '\n']);
+fprintf(fid,['module coeff_data2_2', '\n']);
+fprintf(fid,['#(parameter NBITS=',num2str(NBITScoeff),',','\n']);
+fprintf(fid,['  parameter N=',num2str(N),')\n']);
+fprintf(fid,['(output reg [NBITS*N*2-1:0] coeff_data);','\n']);
+fprintf(fid,['initial begin' , '\n']);
+for index=1 : N
+    fprintf( fid , ['coeff_data[' num2str(N*NBITScoeff*2-1-(index-1)*NBITScoeff*2) ' : ' num2str(N*NBITScoeff*2-(index-1)*NBITScoeff*2-NBITScoeff*2) '] = ' num2str(NBITScoeff*2) '''b' coeff1_2(index,:) ';','\n']); 
+end
+fprintf(fid,['end ' , '\n']);
+fprintf(fid,['endmodule ' , '\n']);
+fclose(fid);
+%%%%%%%%%%%%%%%%%
+fid=fopen ('C:\fft-parallel4\verilog\topfftsrcN128\v\coeff_data2_3.v','wt'); %guardo un archivo con los coef. cuantizados de la señal filtrada de salida
+fprintf(fid,['`timescale 1ns / 1ps' , '\n']);
+fprintf(fid,['module coeff_data2_3', '\n']);
+fprintf(fid,['#(parameter NBITS=',num2str(NBITScoeff),',','\n']);
+fprintf(fid,['  parameter N=',num2str(N),')\n']);
+fprintf(fid,['(output reg [NBITS*N*2-1:0] coeff_data);','\n']);
+fprintf(fid,['initial begin' , '\n']);
+for index=1 : N
+    fprintf( fid , ['coeff_data[' num2str(N*NBITScoeff*2-1-(index-1)*NBITScoeff*2) ' : ' num2str(N*NBITScoeff*2-(index-1)*NBITScoeff*2-NBITScoeff*2) '] = ' num2str(NBITScoeff*2) '''b' coeff1_2(index,:) ';','\n']); 
+end
+fprintf(fid,['end ' , '\n']);
+fprintf(fid,['endmodule ' , '\n']);
+fclose(fid);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- verilog/topfftsrcN128/v/topfft.v
+++ verilog/topfftsrcN128/v/topfft.v
@@ -37,25 +37,198 @@ module topfft
       input rst);
       
       
+     wire [NBITS*2-1:0] in_a_sat0_0_up;
+     wire [NBITS*2-1:0] in_a_sat0_0_down;
+     wire [NBITS*2-1:0] in_a_sat0_1_up;
+     wire [NBITS*2-1:0] in_a_sat0_1_down;
       
+     wire [15*2-1:0] sat0_a_blqIII_0_up;
+     wire [15*2-1:0] sat0_a_blqIII_0_down;
+     wire [15*2-1:0] sat0_a_blqIII_1_up;
+     wire [15*2-1:0] sat0_a_blqIII_1_down;
+
+
+     wire [16*2-1:0] blqIII_0_a_m_up;
+     wire [16*2-1:0] blqIII_0_a_m_down;
+     wire [16*2-1:0] blqIII_1_a_m_up;
+     wire [16*2-1:0] blqIII_1_a_m_down;
+       
+     wire ctrl_Blq_BFIII;  
+     wire coeffCMStage3_en;
+                        
+     wire [28*2-1:0] m_a_blqIV_0_up;
+     wire [28*2-1:0] m_a_blqIV_0_down;
+     wire [28*2-1:0] m_a_blqIV_1_up;
+     wire [28*2-1:0] m_a_blqIV_1_down;                   
+                 
+                        
+     wire coefficientes2_0;
+     wire coefficientes2_1;               
+     wire coefficientes2_2;
+     wire coefficientes2_3;
+     
+     wire coeffw2_0en;
+     wire coeffw2_1en;
+     wire coeffw2_2en;
+     wire coeffw2_3en;
+                    
+                        
+            
+      assign in_a_sat0_0_up   = fftIn0_up;
+      assign in_a_sat0_0_down = fftIn0_down;
+      assign in_a_sat0_1_up   = fftIn1_up;
+      assign in_a_sat0_1_down = fftIn1_down;
+                
+////Desde la entrada al primer cuantizador sat0      
 topfft_in_a_sat0
    #(.NBITS(NBITS),
      .NBITScoeff(NBITScoeff),
      .N(32)) 
     u_topfft_in_a_sat0
     
-     (.fftOut0_up(fftOut0_up),
-      .fftOut0_down(fftOut0_down),
-      .fftOut1_up(fftOut1_up),
-      .fftOut1_down(fftOut1_down),
+     (.fftOut0_up(sat0_a_blqIII_0_up),
+      .fftOut0_down(sat0_a_blqIII_0_down),
+      .fftOut1_up(sat0_a_blqIII_1_up),
+      .fftOut1_down(sat0_a_blqIII_1_down),
       
-      .fftIn0_up(fftIn0_up),
-      .fftIn0_down(fftIn0_down),
-      .fftIn1_up(fftIn1_up),
-      .fftIn1_down(fftIn1_down),
+      .fftIn0_up(in_a_sat0_0_up),
+      .fftIn0_down(in_a_sat0_0_down),
+      .fftIn1_up(in_a_sat0_1_up),
+      .fftIn1_down(in_a_sat0_1_down),
       .clk(clk),
       .rst(rst));
+ /////////////////////
+ 
       
+
+    assign fftOut0_up = sat0_a_blqIII_0_up;
+    assign fftOut0_down = sat0_a_blqIII_0_down;
+    assign fftOut1_up = sat0_a_blqIII_1_up;
+    assign fftOut1_down = sat0_a_blqIII_1_down;
+      
+   
+   
+   //////////Sat 0 a Sat 1
+   
+   
+   
+   Blq
+#(8+16,8+16,15)
+     Blq_BFIII_0
+      (.BlqOut_up      (blqIII_0_a_m_up),
+       .BlqOut_down  (blqIII_0_a_m_down),
+       .BlqIn_up       (sat0_a_blqIII_0_up),
+       .BlqIn_down   (sat0_a_blqIII_0_down),
+       .clk(clk),
+       .rst(rst),
+       .ctrl(ctrl_Blq_BFIII)); 
+    
+
+    
+Blq
+#(8+16,8+16,15)
+     Blq_BFIII_1
+      (.BlqOut_up       (blqIII_1_a_m_up),
+       .BlqOut_down   (blqIII_1_a_m_down),
+       .BlqIn_up       (sat0_a_blqIII_1_up),
+       .BlqIn_down   (sat0_a_blqIII_1_down),
+       .clk(clk),
+       .rst(rst),
+       .ctrl(ctrl_Blq_BFIII)); 
+   
+      
+   
+////////////////Control sw
+
+contador
+ #(8+16) 
+   control_Blq_BFII_0
+        (.clk_out(ctrl_Blq_BFIII),
+         .clk(clk),
+         .rst(!coeffCMStage3_en));  
+
+/////////////////Productos full
+
+assign coeffw2_0en=coeffCMStage3_en;
+assign coeffw2_1en=coeffCMStage3_en;
+assign coeffw2_2en=coeffCMStage3_en;
+assign coeffw2_3en=coeffCMStage3_en;
+   
+  //producto 2_0
+ multip
+ #(16,NBITScoeff)
+       M2_0
+       (.result(m_a_blqIV_0_up),
+        .muestra(blqIII_0_a_m_up),
+        .coeff(coefficientes2_0));      
+        
+  coeff2_0
+ #(NBITScoeff,N)
+      Mcoeff2_0
+     (.coeff_out(coefficientes2_0),
+      .clk(clk),
+      .rst(!coeffw2_0en));      
+        
+        
+
+   
+   
+     //producto 2_1
+ multip
+ #(16,NBITScoeff)
+       M2_1
+       (.result(m_a_blqIV_0_down),
+        .muestra(blqIII_0_a_m_down),
+        .coeff(coefficientes2_1));      
+ coeff2_1
+ #(NBITScoeff,N)
+      Mcoeff2_1
+     (.coeff_out(coefficientes2_1),
+      .clk(clk),
+      .rst(!coeffw2_1en));
+   
+   
+   
+   
+   
+     //producto 2_2
+ multip
+ #(16,NBITScoeff)
+       M2_2
+       (.result(m_a_blqIV_1_up),
+        .muestra(blqIII_1_a_m_up),
+        .coeff(coefficientes2_2));      
+coeff2_2
+ #(NBITScoeff,N)
+      Mcoeff2_2
+     (.coeff_out(coefficientes2_2),
+      .clk(clk),
+      .rst(!coeffw2_2en));  
+             
+        
+   
+   
+     //producto 2_3
+ multip
+ #(16,NBITScoeff)
+       M2_3
+       (.result(m_a_blqIV_1_down),
+        .muestra(blqIII_1_a_m_down),
+        .coeff(coefficientes2_3));      
+   
+coeff2_3
+ #(NBITScoeff,N)
+      Mcoeff2_3
+     (.coeff_out(coefficientes2_3),
+      .clk(clk),
+      .rst(!coeffw2_3en));     
+   
+    
+
+
+
+
+   
       
       
  
