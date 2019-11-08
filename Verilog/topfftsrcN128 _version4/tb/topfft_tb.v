2f3df9b4f8f4233abf2835bce974ac31d2a738a0
JJWK <JJWK@users.noreply.github.com>
2019-11-08 12:33:35 -0300

    V4 completa

---
+++ Verilog/topfftsrcN128 _version4/tb/topfft_tb.v
@@ -0,0 +1,206 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.08.2019 16:41:35
+// Design Name: 
+// Module Name: topfft_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module topfft_tb();
+
+
+`define NULL 0
+    //Para escaneo de entrada obtenia de Matlab
+integer data_in; // file handler
+integer scan_in0; // file handler
+integer scan_in1; // file handler
+integer scan_in2; // file handler
+integer scan_in3; // file handler
+
+    //Para escaneo de archivo de salida de Matlab
+integer data_out; // file handler
+integer scan_out0; // file handler
+integer scan_out1; // file handler
+integer scan_out2; // file handler
+integer scan_out3; // file handler
+
+  
+    
+    
+parameter NBITS=10;
+parameter NBITScoeff=NBITS+1;
+parameter NBITS_out = 10;
+parameter N=32;   
+
+
+reg clk = 0;
+reg rst;  
+reg rst_tb;
+reg rst_file;
+ 
+reg [NBITS*2-1:0] fftIn0_up;
+reg [NBITS*2-1:0] fftIn0_down;
+reg [NBITS*2-1:0] fftIn1_up;
+reg [NBITS*2-1:0] fftIn1_down;
+ 
+wire [NBITS_out*2-1:0] fftOut0_up;
+wire [NBITS_out*2-1:0] fftOut0_down;
+wire [NBITS_out*2-1:0] fftOut1_up;
+wire [NBITS_out*2-1:0] fftOut1_down;
+
+reg [NBITS_out*2-1:0] file_fftOut0_up;
+reg [NBITS_out*2-1:0] file_fftOut0_down;
+reg [NBITS_out*2-1:0] file_fftOut1_up;
+reg [NBITS_out*2-1:0] file_fftOut1_down;
+    
+/*reg comp_fftOut0_up;
+reg comp_fftOut0_down;
+reg comp_fftOut1_up;
+reg comp_fftOut1_down;*/
+
+wire comp_fftOut0_up;
+wire comp_fftOut0_down;
+wire comp_fftOut1_up;
+wire comp_fftOut1_down;
+    
+//-- Generador de reloj. Periodo 2 unidades
+always #1 clk = ~clk;
+        
+topfft#(
+.NBITS(NBITS),
+.NBITScoeff(NBITScoeff),
+.NBITS_out(NBITS_out))
+  dut(
+    .fftIn0_up(fftIn0_up),
+    .fftIn0_down(fftIn0_down),
+    .fftIn1_up(fftIn1_up),
+    .fftIn1_down(fftIn1_down),
+    .fftOut0_up(fftOut0_up),
+    .fftOut0_down(fftOut0_down),
+    .fftOut1_up(fftOut1_up),
+    .fftOut1_down(fftOut1_down),
+    .clk(clk),
+    .rst(rst)
+  );
+    
+
+
+
+
+
+
+
+
+
+
+initial begin
+
+data_in = $fopen("Input128.dat","r");  //lectura de entrada obtenida de Matlab
+if (data_in == `NULL) begin
+        $display("Input128.dat NULL");
+         $finish;
+    end
+    
+data_out = $fopen("Output128.dat","r"); //lectura de salida desde Matlab para comparar con la salida de verilog   
+if (data_out == `NULL) begin
+        $display("Output128.dat NULL");
+         $finish;
+    end    
+
+ rst_tb  = 1'b1;
+ #5 rst_tb  = 1'b0;
+    rst_file=1'b1;
+ 
+#14 rst_file=1'b0; //para alinear archivo con salida y poder comparar
+
+ fftIn0_up   ={NBITS*2{1'b0}} ;
+ fftIn0_down ={NBITS*2{1'b0}};
+ fftIn1_up   ={NBITS*2{1'b0}};
+ fftIn1_down ={NBITS*2{1'b0}};
+
+end
+
+
+
+always @(posedge clk) begin
+   if(rst_tb) begin
+      rst  = 1'd1;
+   #5 rst  = 1'd0;
+    fftIn0_up   ={NBITS*2{1'b0}} ;
+    fftIn0_down ={NBITS*2{1'b0}};
+    fftIn1_up   ={NBITS*2{1'b0}};
+    fftIn1_down ={NBITS*2{1'b0}};
+             end 
+        else if(!$feof(data_in))  begin
+             scan_in0 = $fscanf(data_in, "%b\n", fftIn0_up);
+             scan_in1 = $fscanf(data_in, "%b\n", fftIn0_down);
+             scan_in2 = $fscanf(data_in, "%b\n", fftIn1_up);
+             scan_in3 = $fscanf(data_in, "%b\n", fftIn1_down);
+        end
+             else begin
+             fftIn0_up  ={NBITS*2{1'bz}};
+             fftIn0_down={NBITS*2{1'bz}};
+             fftIn1_up  ={NBITS*2{1'bz}};
+             fftIn1_down={NBITS*2{1'bz}};
+             
+             #50;
+              
+              $finish;
+        end
+ end
+
+
+
+
+
+
+always @(posedge clk) begin
+   if(rst_file) begin
+    file_fftOut0_up   ={NBITS_out*2{1'b0}};
+    file_fftOut0_down ={NBITS_out*2{1'b0}};
+    file_fftOut1_up   ={NBITS_out*2{1'b0}};
+    file_fftOut1_down ={NBITS_out*2{1'b0}};
+             end 
+        else if(!$feof(data_out))  begin
+             scan_out0 = $fscanf(data_out, "%b\n", file_fftOut0_up);
+             scan_out1 = $fscanf(data_out, "%b\n", file_fftOut0_down);
+             scan_out2 = $fscanf(data_out, "%b\n", file_fftOut1_up);
+             scan_out3 = $fscanf(data_out, "%b\n", file_fftOut1_down);
+             end
+             
+              else begin
+    file_fftOut0_up   ={NBITS_out*2{1'b0}};
+    file_fftOut0_down ={NBITS_out*2{1'b0}};
+    file_fftOut1_up   ={NBITS_out*2{1'b0}};
+    file_fftOut1_down ={NBITS_out*2{1'b0}};
+             
+        end
+ end
+
+/////////Comparador de salidas 
+assign comp_fftOut0_up  = (fftOut0_up   === file_fftOut0_up  ) ? 1'b1 : 1'b0;
+assign comp_fftOut0_down= (fftOut0_down === file_fftOut0_down) ? 1'b1 : 1'b0;
+assign comp_fftOut1_up  = (fftOut1_up   === file_fftOut1_up  ) ? 1'b1 : 1'b0;
+assign comp_fftOut1_down= (fftOut1_down === file_fftOut1_down) ? 1'b1 : 1'b0;
+
+
+
+/////////////// Fin de comparador de salidas 
+
+
+
+endmodule
---
+++ Verilog/topfftsrcN128 _version4/v/BFj.v
@@ -0,0 +1,91 @@
+`timescale 1ns / 1ps
+//`include "def.v"
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.07.2019 14:35:16
+// Design Name: 
+// Module Name: BF
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BFj
+    #(parameter NBITS = 10 )
+    (output [(NBITS+1)*2-1:0]   BFOut_up,
+     output [(NBITS+1)*2-1:0] BFOut_down,
+     input  [NBITS*2-1:0]        BFIn_up,
+     input  [NBITS*2-1:0]      BFIn_down,
+     input  twd);
+    
+    
+    //Separacion parte real e imaginaria 
+wire [NBITS-1:0]   q_up_r ;
+wire [NBITS-1:0]   q_up_i ;
+
+
+wire  [NBITS-1:0] q_down_r;
+wire [NBITS-1:0] q_down_i;
+    
+wire signed [NBITS+1-1:0]   sumOut_up_r; //Salida de sumador
+wire signed [NBITS+1-1:0]   sumOut_up_i; //Salida de sumador
+reg signed [NBITS+1-1:0] sumOut_down_r; //Salida de sumador
+reg signed [NBITS+1-1:0] sumOut_down_i; //Salida de sumador   
+
+wire signed [NBITS+1-1:0] aux_subs_r;
+wire signed [NBITS+1-1:0] aux_subs_i;
+wire signed [NBITS+1-1:0] aux_subs_ri;
+
+
+    
+assign q_up_r   = BFIn_up[NBITS*2-1:NBITS]; //Real
+assign q_up_i   = BFIn_up[NBITS-1:0];       //Img
+  
+assign q_down_r = BFIn_down[NBITS*2-1:NBITS]; //Real
+assign q_down_i = BFIn_down[NBITS-1:0]; //Img
+
+//conectar los wire de salida del sumador 
+
+
+
+assign sumOut_up_r = $signed(q_up_r) + $signed(q_down_r);
+assign sumOut_up_i = $signed(q_up_i) + $signed(q_down_i);
+
+assign aux_subs_r =  $signed(q_up_r) - $signed(q_down_r);
+assign aux_subs_i =  $signed(q_up_i) - $signed(q_down_i);  
+
+assign aux_subs_ri = $signed(q_down_r) - $signed(q_up_r);  
+
+always @(*) begin
+if (twd) begin
+sumOut_down_r =  aux_subs_r; 
+sumOut_down_i =  aux_subs_i;  
+end else begin
+sumOut_down_r =  aux_subs_i; 
+sumOut_down_i =  aux_subs_ri;  
+end
+end
+
+
+
+assign BFOut_up   =     {sumOut_up_r,sumOut_up_i};    
+
+assign BFOut_down = {sumOut_down_r,sumOut_down_i}; 
+
+      
+      
+    
+    
+endmodule
+
